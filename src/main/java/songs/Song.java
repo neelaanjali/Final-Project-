f48035a76de76ab0a738bde7c6b07970b7256264
Neela Hanumanthiah <neelahanumanthiah@mu-584783.dhcp.missouri.edu>
2024-04-19 14:50:52 -0500

    Added private String songName, private String artistName, private String length, Getters and setters to Song class

--- src/main/java/songs/Song.java
+++ src/main/java/songs/Song.java
@@ -1,5 +1,29 @@
 package songs;
 
 public class Song {
+	    
+		private String songName;
+	    private String artistName;
+	    private String length;
+		
+	    
+	    public String getSongName() {
+			return songName;
+		}
+		public void setSongName(String songName) {
+			this.songName = songName;
+		}
+		public String getArtistName() {
+			return artistName;
+		}
+		public void setArtistName(String artistName) {
+			this.artistName = artistName;
+		}
+		public String getLength() {
+			return length;
+		}
+		public void setLength(String length) {
+			this.length = length;
+		}
 
 }
