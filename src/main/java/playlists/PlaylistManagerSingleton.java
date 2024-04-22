d8d604034a59e318685301c54ce238697234d8e7
riannaellis <riannaellis@pcp002692pcs.missouri.edu>
2024-04-22 10:58:08 -0500

    PlaylistManagerSingleton property, constructor, getInstance()
    Declared the properties, constructor, and getInstance() method for
    PlaylistManagerSingleton class

--- src/main/java/playlists/PlaylistManagerSingleton.java
+++ src/main/java/playlists/PlaylistManagerSingleton.java
@@ -1,5 +1,24 @@
 package playlists;
 
-public class PlaylistManagerSingleton {
+import java.util.ArrayList;
 
+import songs.Song;
+
+public class PlaylistManagerSingleton {
+	
+	private static PlaylistManagerSingleton instance;
+	public ArrayList<Song> songs;
+	
+	public PlaylistManagerSingleton() {
+		this.songs = new ArrayList<Song>();
+	}
+	
+	public static PlaylistManagerSingleton getInstance() {
+		//create the instance if it doesn't exist yet
+        if (instance == null) {
+            instance = new PlaylistManagerSingleton();
+        }
+        return instance;
+	}
+	
 }
