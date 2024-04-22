43938f90aff4a01f7d10a39b598a71e56e8322b3
riannaellis <riannaellis@pcp002692pcs.missouri.edu>
2024-04-22 11:18:59 -0500

    PlaylistManagerSingleton: created addSong() method
    Asks the user to input information about a new song then adds it to
    their playlist.

--- src/main/java/playlists/PlaylistManagerSingleton.java
+++ src/main/java/playlists/PlaylistManagerSingleton.java
@@ -1,16 +1,17 @@
 package playlists;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import songs.Song;
 
 public class PlaylistManagerSingleton {
 	
 	private static PlaylistManagerSingleton instance;
-	public ArrayList<Song> songs;
+	public ArrayList<Song> playlist;
 	
 	public PlaylistManagerSingleton() {
-		this.songs = new ArrayList<Song>();
+		this.playlist = new ArrayList<Song>();
 	}
 	
 	public static PlaylistManagerSingleton getInstance() {
@@ -21,4 +22,34 @@ public class PlaylistManagerSingleton {
         return instance;
 	}
 	
+	public void addSong() {
+		Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the name of the song
+        System.out.print("Enter the song name: ");
+        // Read the song name
+        String songName = scanner.nextLine();
+
+        // Prompt the user to enter the artist
+        System.out.print("Enter the artist: ");
+        // Read the artist name
+        String artist = scanner.nextLine();
+        
+        // Prompt the user to enter the length
+        System.out.print("Enter the length (MM:SS format): ");
+        // Read the song name
+        String stringLength = scanner.nextLine();
+        //Parse length to int
+        String[] time = stringLength.split(":");
+        int length = (Integer.parseInt(time[0]) * 60) + Integer.parseInt(time[1]);
+        
+        // Create a new Song instance with the input
+        Song newSong = new Song(songName, artist, length);
+        
+        // Add the song to the playlist
+        playlist.add(newSong);
+
+        // Don't forget to close the Scanner object to prevent resource leaks
+        scanner.close();
+	}	
 }
