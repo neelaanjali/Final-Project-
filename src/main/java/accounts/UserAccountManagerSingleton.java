2e886c8ee84b31707e5955c6c5c941fb1a3ed9dc
jxie26 <jessxie54@gmail.com>
2024-04-22 21:53:39 -0500

    Implemented readFromFile() in UserAccountManagerSingleton
    Implemented readFromFile(), which reads in the userAccounts.csv file,
    and included the file reading imports

--- src/main/java/accounts/UserAccountManagerSingleton.java
+++ src/main/java/accounts/UserAccountManagerSingleton.java
@@ -1,5 +1,8 @@
 package accounts;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 /**
@@ -10,6 +13,7 @@ public class UserAccountManagerSingleton {
 	private ArrayList<String> passwords;
 	private ArrayList<UserAccount> accounts;
 	private static UserAccountManagerSingleton instance;
+	private final static String userAccountsFile = "userAccounts.csv";
 
 	//constructor:
 	private UserAccountManagerSingleton() { 
@@ -49,7 +53,53 @@ public class UserAccountManagerSingleton {
 	 * @author jxie26
 	 * @return boolean
 	 */
-	public boolean readFromFile() {
-		return false;
+	private boolean readFromFile() {
+		try {
+			//open the file
+			FileReader fr = new FileReader(userAccountsFile);
+			BufferedReader br = new BufferedReader(fr);
+			
+			br.readLine(); 	//skip the first line (headers) in the file
+			
+			String[] line;
+			String unsplitLine;
+			
+			//loop through each line in the file (each line is an account)
+			unsplitLine = br.readLine();
+			while(unsplitLine != null) {
+				line = unsplitLine.split(",");
+				
+				//check that line was correctly split into 2 parts (username & pw)
+				if(line.length != 2) {
+					break;
+				}
+				
+				//read each line into usernames and passwords ArrayLists
+				String username = line[0].trim();
+				String password = line[1].trim();
+				
+				this.usernames.add(username);
+				this.passwords.add(password);
+				
+				unsplitLine = br.readLine();
+			}
+			
+			//close file and buffered readers
+			br.close();
+			fr.close();
+			return true;
+		}
+		
+		catch (FileNotFoundException e) {
+			System.out.println("The file could not be opened.");
+			e.printStackTrace();
+			return false;
+		}
+		
+		catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
+	
 }
