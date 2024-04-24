ed0285cd293b612f24ead6e0710e4158aec9ebdd
riannaellis <riannaellis@riannas-mbp.dhcp.missouri.edu>
2024-04-24 15:28:12 -0500

    Moved addSong() and removeSong() to Playlist
    I took the methods addSong() and removeSong() and moved them to Playlist
    instead of PlaylistManagerSingleton.

--- src/main/java/playlists/Playlist.java
+++ src/main/java/playlists/Playlist.java
@@ -1,6 +1,7 @@
 package playlists;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import songs.Song;
 
@@ -19,6 +20,54 @@ public class Playlist {
 		this.author = author;
 		this.playlistName = playlistName;
 	}
+	
+	public void addSong() {
+		Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the name of the song
+        System.out.print("Enter the song name: ");
+        // Read the song name
+        String songName = scanner.nextLine();
+
+        // Prompt the user to enter the artist
+        System.out.print("Enter the artist: ");
+        // Read the artist name
+        String artist = scanner.nextLine();
+        
+        // Prompt the user to enter the length
+        System.out.print("Enter the length (MM:SS format): ");
+        // Read the song name
+        String stringLength = scanner.nextLine();
+        //Parse length to int
+        String[] time = stringLength.split(":");
+        int length = (Integer.parseInt(time[0]) * 60) + Integer.parseInt(time[1]);
+        
+        // Create a new Song instance with the input
+        Song newSong = new Song(songName, artist, length);
+        
+        // Add the song to the playlist
+        songs.add(newSong);
+
+        // Don't forget to close the Scanner object to prevent resource leaks
+        scanner.close();
+	}	
+	
+	public void removeSong() {
+		Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter the name of the song they want to delete
+        System.out.print("Enter the song name you'd like to delete: ");
+        // Read the song name
+        String songName = scanner.nextLine();	
+		
+		for (Song song : songs) {
+			// If the song equals the target song, it removes it
+            if (song.getSongName() == songName) {
+                songs.remove(song);
+            }
+        }
+	}
+	
 	public ArrayList<Song> getSongs() {
 		return songs;
 	}
--- src/main/java/playlists/PlaylistManagerSingleton.java
+++ src/main/java/playlists/PlaylistManagerSingleton.java
@@ -47,43 +47,4 @@ public class PlaylistManagerSingleton {
             e.printStackTrace();
         }
     }
-	public void addSong() {
-		Scanner scanner = new Scanner(System.in);
-
-        // Prompt the user to enter the name of the song
-        System.out.print("Enter the song name: ");
-        // Read the song name
-        String songName = scanner.nextLine();
-
-        // Prompt the user to enter the artist
-        System.out.print("Enter the artist: ");
-        // Read the artist name
-        String artist = scanner.nextLine();
-        
-        // Prompt the user to enter the length
-        System.out.print("Enter the length (MM:SS format): ");
-        // Read the song name
-        String stringLength = scanner.nextLine();
-        //Parse length to int
-        String[] time = stringLength.split(":");
-        int length = (Integer.parseInt(time[0]) * 60) + Integer.parseInt(time[1]);
-        
-        // Create a new Song instance with the input
-        Song newSong = new Song(songName, artist, length);
-        
-        // Add the song to the playlist
-        playlist.add(newSong);
-
-        // Don't forget to close the Scanner object to prevent resource leaks
-        scanner.close();
-	}	
-	
-	public void removeSong(Song removedSong) {
-		for (Song song : playlist) {
-			// If the song equals the target song, it removes it
-            if (song.equals(removedSong)) {
-                playlist.remove(song);
-            }
-        }
-	}
 }
