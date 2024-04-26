d8a0f0135ecef9b9026676b93e05ce6b41c29ce3
JosephHargus <jbh7qq@umsystem.edu>
2024-04-26 11:20:25 -0500

    StatusCode Enum
    Enum contains potential status codes that can be returned by methods.
    This will allow for better testing.

---
+++ src/main/java/program/StatusCode.java
@@ -0,0 +1,14 @@
+package program;
+
+
+/**
+ * Enum representing potential status codes to be returned by methods
+ * @author hargu
+ */
+public enum StatusCode {
+    SUCCESS,			//method executed successfully
+    FAILURE,			//method failed
+    EXCEPTION,			//method failed with exception
+    INVALID_INPUT,		//method failed due to invalid input
+    NOT_FOUND,			//the object could not be found
+}
