a49f9d71704b6d1f160f66bd42e649cd1d042c4f
riannaellis <riannaellis@pcp003233pcs.dhcp.missouri.edu>
2024-05-02 17:59:25 -0500

    Created test cases for Playlist getters and setters
    Created test cases for each of the setters and getters in the Playlist
    class

--- src/test/java/playlistsTests/PlaylistTest.java
+++ src/test/java/playlistsTests/PlaylistTest.java
@@ -3,9 +3,12 @@ package playlistsTests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import playlists.Playlist;
 import playlists.Song;
@@ -13,20 +16,98 @@ import program.StatusCode;
 
 public class PlaylistTest {
 	
-	private Playlist playlist;
+	Playlist playlist;
 
 	@BeforeEach
-	private void setUp() {
+	void setUp() {
 		playlist = new Playlist("testAuthor", "testPlaylist", new ArrayList<Song>());
 	}
 	
 	@Test
-	private void testRenamePlaylistWithNull() {
+	void testRenamePlaylistWithNull() {
 		assertEquals(StatusCode.INVALID_INPUT, playlist.renamePlaylist(null));
 	}
 	
 	@Test
-	private void testRenamePlaylistWithValid() {
+	void testRenamePlaylistWithValid() {
 		assertEquals(StatusCode.SUCCESS, playlist.renamePlaylist("testPlaylist"));
+	}
+	
+	@Test
+	void testGetAuthor() {
+		assertEquals("testAuthor", playlist.getAuthor());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideStringForSetAuthor")
+	public void testSetAuthor(String author, StatusCode expected) {
+		assertEquals(expected, playlist.setAuthor(author));
+	}
+	
+	public static Stream<Object[]> provideStringForSetAuthor() {
+		
+		return Stream.of(
+			new Object[]{"testerAuthor", StatusCode.SUCCESS},
+			new Object[]{null, StatusCode.INVALID_INPUT}
+		);
+	}
+	
+	@Test
+	void testGetPlaylistName() {
+		assertEquals("testPlaylist", playlist.getPlaylistName());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideStringForSetPlaylistName")
+	public void testSetPlaylistName(String playlistName, StatusCode expected) {
+		assertEquals(expected, playlist.setPlaylistName(playlistName));
+	}
+	
+	public static Stream<Object[]> provideStringForSetPlaylistName() {
+		
+		return Stream.of(
+			new Object[]{"testerPlaylist", StatusCode.SUCCESS},
+			new Object[]{null, StatusCode.INVALID_INPUT}
+		);
+	}
+	
+	@Test
+	void testGetSumOfRatings() {
+		assertEquals(0, playlist.getSumOfRatings());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideIntForSetSumOfRatings")
+	public void testSetSumOfRatings(int sum, StatusCode expected) {
+		assertEquals(expected, playlist.setSumOfRatings(sum));
+	}
+	
+	public static Stream<Object[]> provideIntForSetSumOfRatings() {
+		
+		return Stream.of(
+			new Object[]{1, StatusCode.SUCCESS},
+			new Object[]{0, StatusCode.SUCCESS},
+			new Object[]{-1, StatusCode.INVALID_INPUT}
+		);
+	}
+	
+	@Test
+	void testGetNumOfRatings() {
+		assertEquals(0, playlist.getNumOfRatings());
+	}
+	
+	@ParameterizedTest
+	@MethodSource("provideIntForSetNumOfRatings")
+	public void testSetNumOfRatings(int num, StatusCode expected) {
+		assertEquals(expected, playlist.setNumOfRatings(num));
+	}
+	
+	public static Stream<Object[]> provideIntForSetNumOfRatings() {
+		
+		return Stream.of(
+			new Object[]{1, StatusCode.SUCCESS},
+			new Object[]{0, StatusCode.SUCCESS},
+			new Object[]{-1, StatusCode.INVALID_INPUT}
+		);
 	}
 }
